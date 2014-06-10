551f00fc5c07e63fac14fd03d7af551ce8d5b293
thankslife <1032474059@qq.com>
2014-06-10 14:15:00 +0800

    testchange

--- test/src/com/lks/test/GitTest.java
+++ test/src/com/lks/test/GitTest.java
@@ -1,7 +1,11 @@
 package com.lks.test;
 
 public class GitTest {
-	
+	public void  testChange(){
+		
+		System.out.println("this is test change test");
+		
+	}
 	public static void main(String[] args) {
 		System.out.println("hello World");
 	}
